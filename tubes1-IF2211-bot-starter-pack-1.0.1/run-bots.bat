009ab0a93adbe9d3fe7e58972fc716626145396d
10-125-Radityalrasyid <raditya.123140125@student.itera.ac.id>
2025-06-01 04:00:04 +0700

    Delete tubes1-IF2211-bot-starter-pack-1.0.1 directory

--- tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-start cmd /k "python main.py --logic jendral --email=koji1@gmail.com --name=closestbase --password=123456 --team etimo"
-start cmd /k "python main.py --logic panglima --email=koji2@gmail.com --name=density --password=123456 --team etimo"
-start cmd /k "python main.py --logic samurai --email=koji3@gmail.com --name=highvalue --password=123456 --team etimo"
-start cmd /k "python main.py --logic perusuh --email=koji5@gmail.com --name=harasser --password==123456 --team etimo"
--- tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 main.py --logic shortdistance --email=koji1@gmail.com --name=petarunk1 --password=123456 --team etimo &
-python3 main.py --logic closestbase --email=koji2@gmail.com --name=petarunk2 --password=123456 --team etimo &
-python3 main.py --logic density2 --email=koji3@gmail.com --name=petarunk3 --password=123456 --team etimo &
-python3 main.py --logic highvalue --email=koji4@gmail.com --name=petarunk4 --password=123456 --team etimo &
